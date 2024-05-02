d63cbd86e2d3af9de0116260d600700e4f3a7229
Anastasia Radchuk <nastasiyaradchuk@gmail.com>
2024-05-01 22:25:02 -0400

    added simple ui

--- fluffy-ui/src/App.jsx
+++ fluffy-ui/src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import './App.css'
 
-const App = () => {
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Home, Register, Login } from './pages'
+
+function App() {
+
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    </div>
+    <>
+      <div className='grid place-content-center py-10'>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   )
 }
 
