2943efc1824644c7a2cb1b37d40e4272e64c4711
Anastasia Radchuk <nastasiyaradchuk@gmail.com>
2024-06-07 09:20:00 -0400

    first commit of java project

---
+++ account/src/test/java/com/fluffy_robot/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluffy_robot.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
